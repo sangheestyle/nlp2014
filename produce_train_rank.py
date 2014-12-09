7b3a7c5df484987863e8762b927106479a644935
azzamsu <azzam.alsudais@colorado.edu>
2014-12-09 00:24:35 -0700

    Adding More Features & Including All Guesses
    I added some features that look for different ranges of scores, and I
    also included the set of all guesses in the training set.

--- produce_train_rank.py
+++ produce_train_rank.py
@@ -7,7 +7,7 @@ from nltk.corpus import wordnet as wn
 from nltk.tokenize import TreebankWordTokenizer
 
 kTOKENIZER = TreebankWordTokenizer()
-TOPGUESSES = 2
+TOPGUESSES = 40
 
 def morphy_stem(word):
     """
@@ -35,19 +35,31 @@ def compose_top_scores(qt_dict, ir_dict, n):
     h = n - (n/2)
     top_guesses = {}
     for i in xrange(h):
-        top_guesses[qt_dict[i][0]] = qt_dict[i][1]
+        top_guesses[qt_dict[i][0]] = float(qt_dict[i][1])
     i = 0
     c = 0
-    
+    for i in xrange(len(ir_dict)):
+        if ir_dict[i][0] not in top_guesses:
+            top_guesses[ir_dict[i][0]] = float(ir_dict[i][1])/10.0
+        if len(top_guesses) == TOPGUESSES:
+            break
+    s = "."
+    while len(top_guesses) < TOPGUESSES:
+        s += "."
+        top_guesses[s] = -1
+    """
     while (i+h) < n:
+    
         while True:
-            if ir_dict[c][0] not in top_guesses:
+            if c < len(ir_dict) or ir_dict[c][0] not in top_guesses:
                 break
             c += 1
+        
         top_guesses[ir_dict[c][0]] = ir_dict[c][1]
         i += 1
-    
+    """
     return top_guesses
+
 def prepare_new_trainingset(train_file):
     train_dict = DictReader(open(train_file, 'r'))
     columns = ['Question ID','Question Text','QANTA Scores','Answer','Sentence Position','IR_Wiki Scores','category','Answer Rank']
@@ -61,13 +73,20 @@ def prepare_new_trainingset(train_file):
         sorted_qd = sorted(qd.items(), key=operator.itemgetter(1), reverse=True)
         sorted_wd = sorted(wd.items(), key=operator.itemgetter(1), reverse=True)
         dict = compose_top_scores(sorted_qd, sorted_qd, TOPGUESSES)
-        sorted_scores = sorted(dict.items(), key=operator.itemgetter(1), reverse=True)
+        #dict = qd
+        #sorted_scores = sorted(dict.items(), key=operator.itemgetter(1), reverse=True)
         ans = r['Answer']
         # Unknown answers (i.e., not among the top TOPGUESSES) are assigned -1
         rank = -1
         for i in xrange(TOPGUESSES):
-            if sorted_scores[i][0] == ans:
-                rank = i
+            if i < 20:
+                if sorted_qd[i][0] == ans:
+                    rank = i
+                    break
+            else:
+                if sorted_wd[i-TOPGUESSES][0] == ans:
+                    rank = i
+                    break
         if rank == -1:
             c += 1
         new_train.writerow({'Question ID':r['Question ID'], 'Question Text':r['Question Text'], 'QANTA Scores':r['QANTA Scores'], 'Answer':ans, 'Sentence Position':r['Sentence Position'], 'IR_Wiki Scores':r['IR_Wiki Scores'],'category':r['category'], 'Answer Rank':rank})
